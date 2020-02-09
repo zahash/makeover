d38cf7f31b777eaf4b3ed0c63b30580cc329b93a
Zahash Baig <zahash.z@gmail.com>
2020-02-09 10:42:38 +0530

    add simple commandline arguments support

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import dash
 import dash_html_components as html
 
-import os
+import sys
 import pandas as pd
 
 from utils import generate_single_column_plots, generate_multi_column_plots
@@ -10,10 +10,9 @@ from config import EXTERNAL_STYLESHEETS, EXTERNAL_SCRIPTS, DATA_DIR
 app = dash.Dash(__name__, external_stylesheets=EXTERNAL_STYLESHEETS,
                 external_scripts=EXTERNAL_SCRIPTS)
 
-dataset_name = 'iris.csv'
-target_column_name = 'species'
+dataset_path = sys.argv[1]
+target_column_name = sys.argv[2]
 
-dataset_path = os.path.join(DATA_DIR, dataset_name)
 df = pd.read_csv(dataset_path)
 
 feat = df.drop(columns=[target_column_name])
@@ -22,7 +21,7 @@ label = df[target_column_name]
 plots = generate_single_column_plots(feat, label) + \
     generate_multi_column_plots(feat, label)
 
-title = html.H1(children=dataset_name, style={
+title = html.H1(children=dataset_path, style={
     'textAlign': 'center', 'fontFamily': 'Montserrat'})
 
 
